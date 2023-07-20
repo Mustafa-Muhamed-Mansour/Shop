9ba136c00e3ef0711517803ecfcda9a97a18d311
Mustafa-Muhamed-Mansour <mostafamansour163@yahoo.com>
2023-07-20 14:43:26 +0300

    remove_api

--- app/src/main/java/com/shop/other/Constant.kt
+++ app/src/main/java/com/shop/other/Constant.kt
@@ -3,7 +3,7 @@ package com.shop.other
 class Constant {
 
     companion object {
-        const val BASE_URL = "https://student.valuxapps.com/"
+        const val BASE_URL = "https://base_url.com/"
         const val END_POINT = "api/products"
     }
 }
